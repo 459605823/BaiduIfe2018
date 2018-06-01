ccb271be0c0ff7e5ba5f8f3ed8ac7a09a73b73a2
beast <35030553+459605823@users.noreply.github.com>
2018-06-01 19:50:36 +0800

    Update tableEvent.js

--- 零基础学院/mis/src/js/tableEvent.js
+++ 零基础学院/mis/src/js/tableEvent.js
@@ -1,10 +1,13 @@
 export {tableEvent};
 import {line} from "./line.js";
 import {bar} from "./bar.js";
+import {fetch,save} from "./localStorage.js";
 import {renderMultipleLines} from "./renderMultipleLines.js";
 
 function tableEvent(table){
   var trs = table.getElementsByTagName("tr");
+  var editTds = table.querySelectorAll(".edit");
+  var editFlag = false;
   for(let i=0,len=trs.length;i<len;i++){
     trs[i].addEventListener("mouseover",function(){
       var newData = [];
@@ -23,7 +26,67 @@ function tableEvent(table){
     })
     trs[i].addEventListener("mouseout",function(){
       //当鼠标离开后重新渲染多条折线图
-      renderMultipleLines()
+      renderMultipleLines();
+      bar.setData(fetch()[0].sale);
     })
   }
-}
+  for(let j=0,len=editTds.length;j<len;j++){
+    editTds[j].onclick = function(e){
+      if(!editFlag){
+        var text = this.innerText;
+        this.setAttribute("class","");
+        this.innerHTML = "<input type = 'text' placeholder="+text+" class='editInput'></input><input type = 'button' value = '确认'></input><input type = 'button' value = '取消'></input>"
+        var editInput = this.querySelector('input[type=text]');
+        editInput.focus();
+        editFlag = true;
+        e.stopPropagation();
+      }
+    }
+  }
+   table.onclick = function(e){
+     if(e.target&&e.target.nodeName.toLowerCase()=='input') {
+     var td = e.target.parentNode;
+     var text = td.querySelector('input[type="text"]');
+     switch(e.target.value) {
+       case '确认':
+         if(text.value == text.placeholder || text.value == '') {
+           td.innerText = text.placeholder;
+         } else if(text.value != text.placeholder && text.value != '' && !isNaN(text.value)){
+           td.innerText = text.value;
+         } else {
+           alert('不能含有非数字');
+           break;
+         }
+         editFlag = false;
+         td.setAttribute('class','edit');
+         break;
+       case '取消':
+         td.innerText = text.placeholder;
+         editFlag = false;
+         td.setAttribute('class','edit');
+         break;
+     }
+    }
+   }
+   //点击页面其他位置取消输入如状态
+   document.onclick = function(e) {
+		var td_input  = table.querySelector('input[value="取消"]');
+		if(td_input!=null&&e.target&&e.target.nodeName.toLowerCase()!='input') {
+			td_input.click();
+		}
+	}
+   table.onkeydown = function(e) {
+		switch(e.keyCode) {
+      //ESC
+			case 27:
+				var input = table.querySelector('input[value="取消"]');
+				input.click();
+				break;
+      //回车
+			case 13:
+				var input = table.querySelector('input[value="确认"]');
+				input.click();
+				break;
+		}
+	}
+ }
