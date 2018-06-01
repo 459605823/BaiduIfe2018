6ae824cb3e24a55a5d1cf2f24479eb31872967c0
beast <35030553+459605823@users.noreply.github.com>
2018-06-01 19:46:20 +0800

    Update webpack.config.js

--- 零基础学院/mis/webpack.config.js
+++ 零基础学院/mis/webpack.config.js
@@ -11,6 +11,30 @@ const config = {
     filename:'bundle.js',
     path:path.join(__dirname,'dist')
   },
+  module:{
+    rules:[
+      {
+        test:/\.(gif|jpg|jpeg|png|svg)$/,
+        use:[
+          {
+            //处理图片有:file-loader,url-loader,image-webpack-loader
+            loader:"url-loader",
+            options:{
+              limit:1024,
+              name:'[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+          'css-loader'
+        ]
+       }
+    ]
+  },
   plugins:[
     //自动清理插件:每次构建前清理 /dist 文件夹
     new CleanWebpackPlugin(['dist']),
@@ -24,3 +48,4 @@ const config = {
 }
 
 module.exports = config;
+
