7664d3a2cac3c8d1e1620f94d0fe46980003010a
beast <35030553+459605823@users.noreply.github.com>
2018-06-02 13:26:25 +0800

    Update app.js

--- 零基础学院/mis/src/app.js
+++ 零基础学院/mis/src/app.js
@@ -13,16 +13,7 @@ var regionCheckboxWrapper = document.getElementById("region-checkbox-wrapper");
 var productCheckboxWrapper = document.getElementById("product-checkbox-wrapper");
 
 window.onload = function(){
-  var checkboxs = document.querySelectorAll("input[type=checkbox]");
-  //初始默认将表单全选
-  for(let i=0,len=checkboxs.length;i<len;i++){
-    checkboxs[i].checked = true;
-  }
-  //显示所有数据
-  var regionArr = selectedValue(regionCheckboxWrapper);
-  var productArr = selectedValue(productCheckboxWrapper);
-  tableWrapper.innerHTML=renderNewTable(regionArr,productArr,getData(regionArr,productArr));
-  // renderTable(regionArr,productArr,getData(regionArr,productArr));
+  renderByUrl();
   var saveBtn = document.getElementById("saveData");
   saveBtn.addEventListener("click",function(){
     var checkboxlist = {
@@ -68,83 +59,61 @@ window.onload = function(){
      renderMultipleLines();
      bar.setData(fetch()[0].sale);
   })
-
-  tableEvent(tableWrapper);
+  window.addEventListener("popstate", renderByUrl, false);
   generateCheckbox(regionCheckboxWrapper);
   generateCheckbox(productCheckboxWrapper);
-  bar.render();
-  renderMultipleLines();
 }
 
-// function renderTable(regionArr,productArr,data){
-//   let headerArray1 = ["商品","地区"];
-//   let headerArray2 = ["地区","商品"];
-//   let saleArray = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
-//   let rowspanOff = false;
-//   tableWrapper.innerHTML = "";
-//   var table = document.createElement("table");
-//   table.border = 1;
-//   var tbody = document.createElement("tbody");
-//   table.appendChild(tbody);
-//   tbody.insertRow(0);
-//   //输出表头
-//   for(let i=0;i<2;i++){
-//     tbody.rows[0].insertCell(i);
-//     if(productArr.length==1&&regionArr.length>productArr.length){
-//       tbody.rows[0].cells[i].appendChild(document.createTextNode(headerArray1[i]));
-//     }else if(regionArr.length==1&&productArr.length>regionArr.length){
-//       tbody.rows[0].cells[i].appendChild(document.createTextNode(headerArray2[i]));
-//     }else if(regionArr.length>1&&productArr.length>1){
-//       tbody.rows[0].cells[i].appendChild(document.createTextNode(headerArray1[i]));
-//     }else{
-//       tbody.rows[0].cells[i].appendChild(document.createTextNode(headerArray1[i]));
-//     }
-//   }
-//   for(let i=0,len=saleArray.length;i<len;i++){
-//     tbody.rows[0].insertCell(i+2);
-//     tbody.rows[0].cells[i+2].appendChild(document.createTextNode(saleArray[i]));
-//   }
-//   //遍历数据
-//   for(let i=0,len=data.length;i<len;i++){
-//     tbody.insertRow(i+1);
-//     tbody.rows[i+1].insertCell(0);
-//     if(productArr.length<regionArr.length&&productArr.length==1){
-//       if(!rowspanOff){
-//         tbody.rows[i+1].cells[0].rowSpan = regionArr.length;
-//         tbody.rows[i+1].cells[0].appendChild(document.createTextNode(data[i].product));
-//         rowspanOff = true;
-//       }
-//       tbody.rows[i+1].insertCell(1);
-//       tbody.rows[i+1].cells[1].appendChild(document.createTextNode(data[i].region));
-//     }else if(productArr.length>regionArr.length&&regionArr.length==1){
-//       if(!rowspanOff){
-//         tbody.rows[i+1].cells[0].rowSpan = productArr.length;
-//         tbody.rows[i+1].cells[0].appendChild(document.createTextNode(data[i].region));
-//         rowspanOff = true;
-//       }
-//       tbody.rows[i+1].insertCell(1);
-//       tbody.rows[i+1].cells[1].appendChild(document.createTextNode(data[i].product));
-//     }else if(productArr.length>1&&regionArr.length>1){
-//       if(i%regionArr.length==0){
-//         rowspanOff = false;
-//       }
-//       if(!rowspanOff){
-//         tbody.rows[i+1].cells[0].rowSpan = regionArr.length;
-//         tbody.rows[i+1].cells[0].appendChild(document.createTextNode(data[i].product));
-//         rowspanOff = false;
-//       }
-//       tbody.rows[i+1].insertCell(1);
-//       tbody.rows[i+1].cells[1].appendChild(document.createTextNode(data[i].region));
-//     }else{
-//       tbody.rows[i+1].cells[0].rowSpan = 1;
-//       tbody.rows[i+1].cells[0].appendChild(document.createTextNode(data[i].product));
-//       tbody.rows[i+1].insertCell(1);
-//       tbody.rows[i+1].cells[1].appendChild(document.createTextNode(data[i].region));
-//     }
-//     for(let j=0;j<12;j++){
-//       tbody.rows[i+1].insertCell(j+2);
-//       tbody.rows[i+1].cells[j+2].appendChild(document.createTextNode(data[i].sale[j]));
-//     }
-//   }
-//   tableWrapper.appendChild(table);
-//  }
+function renderByUrl(){
+  //页面第一次加载时显示全部信息
+  if(typeof window.location.href.split("?")[1] == "undefined"){
+    var checkboxs = document.querySelectorAll("input[type=checkbox]");
+    //初始默认将表单全选
+    for(let i=0,len=checkboxs.length;i<len;i++){
+      checkboxs[i].checked = true;
+    }
+    //显示所有数据
+    var regionArr = selectedValue(regionCheckboxWrapper);
+    var productArr = selectedValue(productCheckboxWrapper);
+    tableWrapper.innerHTML=renderNewTable(regionArr,productArr,getData(regionArr,productArr));
+    tableEvent(tableWrapper);
+    bar.render();
+    renderMultipleLines();
+  }else{
+    var regionChecks = document.querySelectorAll("input[name=region]");
+    var regionSelectAll = regionCheckboxWrapper.querySelector("#selectAll");
+    var productChecks = document.querySelectorAll("input[name=product]");
+    var productSelectAll = productCheckboxWrapper.querySelector("#selectAll");
+    //获取此时url中解码后的信息
+    var regionQuery = decodeURIComponent(window.location.href.split("?")[1].split("-")[0]).split(",");
+    var productQuery = decodeURIComponent(window.location.href.split("?")[1].split("-")[1]).split(",");
+    for(let i=0;i<regionChecks.length;i++){
+      if(regionQuery.indexOf(regionChecks[i].value) != -1){
+        regionChecks[i].checked = true;
+      }else{
+        regionChecks[i].checked = false;
+      }
+    }
+    if(regionQuery.length === 3){
+      regionSelectAll.checked = true;
+    }else{
+      regionSelectAll.checked = false;
+    }
+    for(let i=0;i<productChecks.length;i++){
+      if(productQuery.indexOf(productChecks[i].value) != -1){
+        productChecks[i].checked = true;
+      }else{
+        productChecks[i].checked = false;
+      }
+    }
+    if(productQuery.length === 3){
+      productSelectAll.checked = true;
+    }else{
+      productSelectAll.checked = false;
+    }
+    tableWrapper.innerHTML=renderNewTable(regionQuery,productQuery,getData(regionQuery,productQuery));
+    tableEvent(tableWrapper);
+    bar.render();
+    renderMultipleLines();
+  }
+}
